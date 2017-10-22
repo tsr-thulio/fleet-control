daf46c0632baf8d507fa8cd4b0344d77b7edc7dd
Thulio Ribeiro <tribeir4@its.jnj.com>
2017-10-22 02:18:47 -0200

    creating backend structure and some dependencies

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require('path');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
+
+app.use(require('./route/route'));
