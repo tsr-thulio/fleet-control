148429bc797c098cfb50362acfc9f2f7deba2542
Thulio Ribeiro <tribeir4@its.jnj.com>
2017-10-22 04:19:34 -0200

    code review problems

--- app/config.js
+++ app/config.js
@@ -4,10 +4,8 @@ module.exports = function($mdThemingProvider) {
 
   var contaAzulBlueMap = $mdThemingProvider.extendPalette('blue', {
     '500': '#2585e9',
-    // 'contrastDefaultColor': 'dark'
   });
 
-  // Register the new color palette map with the name <code>neonRed</code>
   $mdThemingProvider.definePalette('contaAzul', contaAzulBlueMap);
 
   $mdThemingProvider
